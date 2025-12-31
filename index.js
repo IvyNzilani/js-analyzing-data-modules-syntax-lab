fbefdbc4920926826576a9f78eae473d64d416ce
Ivy Nzilani <ivynzilani9@gmail.com>
2025-12-31 04:31:57 -0800

    Add combineUsers function for merging arrays with merge date

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
+// Load datejs
+require('datejs');
+
+// Function to combine users
+function combineUsers(...args) {
+  // Step 2: Initialize return object
+  const combinedObject = {
+    users: []
+  };
+
+  // Step 3 & 4: Loop and merge arrays using spread operator
+  for (let arr of args) {
+    combinedObject.users.push(...arr);
+  }
+
+  // Step 5: Add today's date
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  // Step 7: Return object
+  return combinedObject;
+}
+
+// Export the function (required for Jest tests)
+module.exports = { combineUsers };
 
 
 
 module.exports = {
   ...(typeof combineUsers !== 'undefined' && { combineUsers })
 };
+
+
+
